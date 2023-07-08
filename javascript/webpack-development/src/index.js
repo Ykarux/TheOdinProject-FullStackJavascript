9b9f36aeaa9a0d3af7e08484874f647d6b4f6095
guillaume-giannantonio <guillaume.giannantonio@protonmail.com>
2023-07-08 17:28:42 +0200

    Setup webpack-development

---
+++ javascript/webpack-development/src/index.js
@@ -0,0 +1,18 @@
+import _ from 'lodash';
+import printMe from './print';
+
+function component() {
+	const element = document.createElement('div');
+	const btn = document.createElement('button');
+
+	element.textContent = _.join(['Hello', 'webpack'], ' ');
+	btn.textContent = 'Click me !'
+	btn.onclick = printMe;
+
+	element.appendChild(btn)
+
+
+	return element;
+}
+
+document.body.appendChild(component());
