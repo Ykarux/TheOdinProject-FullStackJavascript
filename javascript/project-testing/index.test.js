6991c38f6d078c5a61311774fba576aeb55470fc
guillaume-giannantonio <guillaume.giannantonio@protonmail.com>
2023-09-08 17:53:52 +0200

    add calculator test

--- javascript/project-testing/index.test.js
+++ javascript/project-testing/index.test.js
@@ -8,3 +8,9 @@ test('reverseString', () => {
 	expect(reverseString('string')).toMatch('gnirts')
 })
 
+test('calculator', () => {
+	expect(calculator.add(1, 2)).toEqual(3)
+	expect(calculator.sub(3, 2)).toEqual(1)
+	expect(calculator.mul(2, 3)).toEqual(6)
+	expect(calculator.div(5, 2)).toBeCloseTo(2.5)
+})
