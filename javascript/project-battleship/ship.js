d2b4cbb4ef5ecd8bb4907b2ffc5e55ea6a04ec0e
guillaume-giannantonio <guillaume.giannantonio@protonmail.com>
2023-09-13 23:10:05 +0200

    add ship

--- javascript/project-battleship/ship.js
+++ javascript/project-battleship/ship.js
@@ -1,3 +1,24 @@
-export default function Ship() {
-	return {}
+function Ship(length) {
+	let hitTimes = 0;
+
+	function hit() {
+		hitTimes++;
+	}
+
+	function isSunk() {
+		return hitTimes === length;
+	}
+
+	return {
+		get length() {
+			return length;
+		},
+		get hitTimes() {
+			return hitTimes;
+		},
+		hit,
+		isSunk,
+	}
 }
+
+module.exports = {Ship}
--- javascript/project-battleship/ship.test.js
+++ javascript/project-battleship/ship.test.js
@@ -1,9 +1,18 @@
-const Ship = require('./ship')
+const {Ship} = require('./ship');
 
 
 test('Ship', () => {
-	const myShip = new Ship(3);
+	const myShip = Ship(3);
 	expect(myShip.length).toBe(3);
 	expect(myShip.hitTimes).toBe(0);
-	expect(myShip.sunk).toBeFalthy();
+})
+
+test('Ship functions', () => {
+	const myShip = Ship(3);
+	expect(myShip.hit()).toBe(1);
+	expect(myShip.isSunk()).toBeFalsy();
+	expect(myShip.hit()).toBe(2);
+	expect(myShip.isSunk()).toBeFalsy();
+	expect(myShip.hit()).toBe(3);
+	expect(myShip.isSunk()).toBeTruthy();
 })
