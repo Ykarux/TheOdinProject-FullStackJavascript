4d4a75d9faa5429f675d5efcf2af27a5958c868a
guillaume-giannantonio <guillaume.giannantonio@protonmail.com>
2023-09-13 23:22:58 +0200

    add gameboard tests

--- javascript/project-battleship/gameboard.test.js
+++ javascript/project-battleship/gameboard.test.js
@@ -4,6 +4,20 @@ const {Ship} = require('./ship');
 test('GameBoard', () => {
 	const myGameBoard = GameBoard(10);
 	expect(myGameBoard.length).toBe(10);
-	const myShip = Ship(3);
-	
+	myGameBoard.placeShip(3, 6, 3);
+	myGameBoard.placeShip(1, 1, 2);
+	expect(myGameBoard.ships).toContain([3, 6, 3])
+	expect(myGameBoard.ships).toContain([1, 1, 2])
+	expect(myGameBoard.hit(3, 9)).toBeFalsy()
+	expect(myGameBoard.hit(8, 5)).toBeFalsy()
+	expect(myGameBoard.areShipsSunk()).toBeFalsy()
+	expect(myGameBoard.hit(1, 1)).toBeTruthy()
+	expect(myGameBoard.hit(1, 2)).toBeTruthy()
+	expect(myGameBoard.hit(1, 3)).toBeFalsy()
+	expect(myGameBoard.hit(3, 6)).toBeTruthy()
+	expect(myGameBoard.areShipsSunk()).toBeFalsy()
+	expect(myGameBoard.hit(3, 7)).toBeTruthy()
+	expect(myGameBoard.shotsList()).toContain([[3, 9, false], [8, 5, false], [1, 1, true], [1, 2, true], [1, 3, false], [3, 6, true], [3, 7, true], [3, 8, true]])
+	expect(myGameBoard.hit(3, 8)).toBeTruthy()
+	expect(myGameBoard.areShipsSunk()).toBeTruthy()
 })
