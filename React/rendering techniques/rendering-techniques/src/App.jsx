bb23b6b0d593286a2839bd729ebcf818bd7f8afa
guillaume-giannantonio <guillaume.giannantonio@protonmail.com>
2023-09-17 22:54:07 +0200

    add rendering techniques

---
+++ React/rendering techniques/rendering-techniques/src/App.jsx
@@ -0,0 +1,44 @@
+import './App.css'
+
+function List(props) {
+
+	if (!props.animals) {
+		return <div>Loading...</div>
+	}
+
+	if (props.animals.length === 0) {
+		return <div>There are no animals in this list!</div>
+	}
+
+	return (
+		<ul>
+			{props.animals.map(animal => {
+				return animal.startsWith('L') ? <li key={animal}>{animal}</li> : null;
+			})}
+		</ul>
+	)
+}
+
+function ListWithAND(props) {
+	return (
+		<ul>
+			{props.animals.map(animal => {
+				return animal.startsWith('L') && <li key={animal}>{animal}</li>;
+			})}
+		</ul>
+	)
+}
+
+function App() {
+
+	const animals = ['Lion', 'Cow', 'Snake', 'Lizard']
+
+	return (
+		<div>
+			<h1>Animals:</h1>
+			<List animals={animals}/>
+		</div>
+	)
+}
+
+export default App
